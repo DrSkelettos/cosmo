03e4395273e2e82a50c983d9d9bc1318d1a132bf
DrSkelettos <saniel7693@gmx.de>
2026-07-17 21:22:50 +0200

    cron fixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,16 @@ set -e
 mkdir -p /var/www/html/storage/logs /var/www/html/database /var/log/supervisor
 chown -R www-data:www-data /var/www/html/storage /var/www/html/database
 
+# Create and set permissions for laravel.log file
+touch /var/www/html/storage/logs/laravel.log
+chown www-data:www-data /var/www/html/storage/logs/laravel.log
+chmod 664 /var/www/html/storage/logs/laravel.log
+
+# Create and set permissions for cron log file
+touch /var/log/cron.log
+chown www-data:www-data /var/log/cron.log
+chmod 664 /var/log/cron.log
+
 # Generate an application key if the environment does not provide one
 if [ -z "$APP_KEY" ]; then
     php artisan key:generate --no-interaction
